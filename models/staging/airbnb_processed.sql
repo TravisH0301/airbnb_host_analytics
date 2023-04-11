f630f3f1fa00013e13dfed544144dabbc9b3cce2
Travis Hong <travis.j.hong@gmail.com>
2023-04-12 01:51:21 +1000

    add query for staging table

--- models/staging/airbnb_processed.sql
+++ models/staging/airbnb_processed.sql
@@ -1,2 +1,93 @@
-select *
-from {{ source('raw_data', 'airbnb_raw') }}
+WITH BASE AS (
+    SELECT
+        last_scraped AS SNAPSHOT_DATE,
+        CAST(
+            RIGHT(
+                listing_url,
+                POSITION('/' IN REVERSE(listing_url)) - 1
+            ) AS INT64
+        ) AS LISTING_ID,
+        host_id AS HOST_ID,
+        DATE_DIFF(
+            'YEAR',
+            host_since,
+            CURRENT_DATE
+        ) AS HOST_YEAR_OF_EXP,
+        CAST(
+            LENGTH(RTRIM(LTRIM(host_about))) - LENGTH(REPLACE(RTRIM(LTRIM(host_about)), ' ', '')) + 1 AS NUMERIC
+        ) AS HOST_ABOUT_WORD_COUNT,
+        host_is_superhost AS IS_HOST_SUPERHOST,
+        host_has_profile_pic AS HAS_HOST_PROFILE_PHOTO,
+        calculated_host_listings_count AS HOST_LISTING_COUNT,
+        latitude AS LISTING_LATITUDE,
+        longitude AS LISTING_LONGITUDE,
+        price AS LISTING_PRICE,
+        PERCENTILE_CONT(price, 0.50) OVER() AS LISTING_PRICE_2Q,
+        PERCENTILE_CONT(price, 0.75) OVER() * 1.5 AS LISTING_PRICE_UPPER,
+        ROUND((30 - availability_30) / 30 * 100, 0) AS LISTING_OCCUPANCY_RATE,
+        number_of_reviews AS LISTING_REVIEW_COUNT,
+        neighbourhood_cleansed AS LISTING_MUNICIPALITY
+    FROM
+        { { source('raw_data', 'airbnb_raw') } }
+    WHERE
+        1 = 1
+        AND host_identity_verified = 't'
+        AND availability_60 <> 0
+        AND availability_90 <> 0
+        AND availability_365 <> 0
+        AND LOWER(neighbourhood_cleansed) IN (
+            'banyule',
+            'bayside',
+            'boroondara',
+            'brimbank',
+            'cardinia',
+            'casey',
+            'darebin',
+            'frankston',
+            'glen eira',
+            'greater dandenong',
+            'hobsons bay',
+            'hume',
+            'kingston',
+            'knox',
+            'manningham',
+            'maribyrnong',
+            'maroondah',
+            'melbourne',
+            'melton',
+            'monash',
+            'moonee valley',
+            'moreland',
+            'mornington peninsula',
+            'nillumbik',
+            'port phillip',
+            'stonnington',
+            'whitehorse',
+            'whittlesea',
+            'wyndham',
+            'yarra',
+            'yarra ranges'
+        )
+        AND LOWER(room_type) = 'entire home/apt'
+)
+SELECT
+    SNAPSHOT_DATE,
+    LISTING_ID,
+    HOST_ID,
+    HOST_YEAR_OF_EXP,
+    HOST_ABOUT_WORD_COUNT,
+    IS_HOST_SUPERHOST,
+    HAS_HOST_PROFILE_PHOTO,
+    HOST_LISTING_COUNT,
+    LISTING_LATITUDE,
+    LISTING_LONGITUDE,
+    LISTING_OCCUPANCY_RATE,
+    LISTING_PRICE,
+    LISTING_REVIEW_COUNT,
+    LISTING_MUNICIPALITY
+FROM
+    BASE
+WHERE
+    1 = 1
+    AND LISTING_PRICE >= LISTING_PRICE_2Q
+    AND LISTING_PRICE <= LISTING_PRICE_UPPER
