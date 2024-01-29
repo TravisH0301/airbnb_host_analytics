b4196054a0e7239d3b1e4c85aca36dc756647883
Travis Hong <travis.j.hong@gmail.com>
2024-01-29 23:15:22 +1100

    add dimension and fact table descriptions

--- src/azure-databricks/data_modelling.py
+++ src/azure-databricks/data_modelling.py
@@ -33,6 +33,46 @@ def main():
         file_path,
         file_type
     )
+
+    # Model Host dimension table
+    """Host dimension table contains host details with
+    the following attributes:
+    - host_id: Host unique identification
+    - is_super_host: Indicator of super host
+    - has_profile_photo: Indicator of profile photo
+    - about_word_count: Host about description word count
+    - year_of_exp: Years of experience in hosting
+    - listing_count: Number of property listed
+    
+    And this dimension follows SCD type 2.
+    """
+
+    # Model Listing dimension table
+    """Listing dimension table contains listing property details
+    with the following attributes:
+    - listing_id: Listing property unique identification
+    - municipality: Suburb
+    - latitude: Latitude
+    - longitude: Latitude
+    - price: Listing price
+    - review_count: Number of reviews
+    
+    And this dimension follows SCD type 2.
+    """
+
+    # Model Occupancy fact table
+    """Occupancy fact table contains occupancy rate within the 
+    next 30 days of the listing properties with the following
+    attributes:
+    - id: Unique identifiation
+    - listing_id: Listing identification
+    - host_id: Host identification
+    - occupancy_rate: Occupancy rate within next 30 days
+    - snapshot_date: Monthly snapshot date
+
+    This table is a monthly snapshot fact table containing 
+    occupancy rate records at monthly intervals.
+    """
     
 
     print("Process has completed.")
