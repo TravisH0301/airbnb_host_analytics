fb78593391c82fca2c11d4da885fd2bb31445e53
Travis Hong <travis.j.hong@gmail.com>
2024-01-31 10:52:41 +1100

    add test script

---
+++ tests/test_ingest_data.py
@@ -0,0 +1,44 @@
+###############################################################################
+# Name: test_ingest_data.py
+# Description: This script conducts unit test and integration test of 
+#              ingest_data.py script.
+# Author: Travis Hong
+# Repository: https://github.com/TravisH0301/azure_airbnb_host_analytics
+###############################################################################
+import yaml
+import sys
+sys.path.append("src/azure-function")
+
+from unittest import TestCase, main
+
+from ingest_data import fetch_data, load_storage_cred, create_dir_client, create_dir_client
+
+
+class test_functions(TestCase):
+    def test_fetch_data(self):
+        """This checks the integration with the API."""
+        url = "http://data.insideairbnb.com/australia/vic/melbourne/2023-03-13" \
+            "/data/listings.csv.gz"
+        df = fetch_data(url)
+        
+        self.assertFalse(df.empty)
+
+    def test_load_storage_cred(self):
+        """This checks functionality of reading YAML file."""
+        # Create mock YAML file
+        mock_data = {
+            "account_name": "abc",
+            "account_key": "123"
+        }
+        mock_yaml_filename = "mock_cred.yaml"
+        with open(mock_yaml_filename, "w") as file:
+            yaml.dump(mock_data, file, default_flow_style=False)
+        # Test function
+        account_cred = load_storage_cred(mock_yaml_filename)
+        
+        self.assertEqual(account_cred, ("abc", "123"))
+
+if __name__ == "__main__":
+    main()
+
+
