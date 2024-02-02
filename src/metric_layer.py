96c57dab0baa54788070eef679dc48fe4eecdd74
Travis Hong <travis.j.hong@gmail.com>
2024-02-03 01:58:51 +1100

    change directory

--- src/metric_layer.py
+++ src/metric_layer.py
@@ -1,7 +1,7 @@
 ###############################################################################
 # Name: metric_layer.py
 # Description: This script creates a metric layer on top of the dimensional
-#              data model and stores into the gold-dev layer of the data 
+#              data model and stores into the gold-dev layer of the data
 #              lakehouse.
 # Author: Travis Hong
 # Repository: https://github.com/TravisH0301/azure_airbnb_host_analytics
